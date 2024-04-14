ad286cdacc700b712016a723c2a66006e9cbad57
mav <email@example.com>
2024-04-14 17:38:23 +0300

    change ref all

--- stores/category.ts
+++ stores/category.ts
@@ -1,23 +1,22 @@
 // stores/counter.js
 import { defineStore } from 'pinia';
 
-export const useCategoryStores = defineStore('category',  () => {
-  const category = ref('all');
-  const pizzas =  useFetch('/api/pizza')
-  const allCategories=computed(()=>{
- const result:string[] =[]
-    if(pizzas.data.value){
-      pizzas.data.value.map((elem: { tag: string[]; })=>result.push(...elem.tag))
-      
-     
+export const useCategoryStores = defineStore('category', () => {
+  const category = ref('Все');
+  const pizzas = useFetch('/api/pizza');
+  const allCategories = computed(() => {
+    const result: string[] = [];
+    if (pizzas.data.value) {
+      pizzas.data.value.map((elem: { tag: string[] }) =>
+        result.push(...elem.tag)
+      );
     }
-    return new Set(result)
+    return new Set(result);
+  });
 
-  })
-  
   function setCategory(payload: string) {
     category.value = payload;
   }
 
-  return { category,allCategories, setCategory };
+  return { category, allCategories, setCategory };
 });
